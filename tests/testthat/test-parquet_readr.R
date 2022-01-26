ae851e758591398deaaf7ecfe9bfb98d8ec50666
John Waller <ftw712@science.domain>
2022-01-26 10:27:04 +0100

    checking covdev

--- tests/testthat/test-parquet_readr.R
+++ tests/testthat/test-parquet_readr.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("simple tests", {
   expect_true(file.exists(file.path("test.parquet")))
   # do not use arrow in unit tests to pass CRAN checks
   # expect_equal(nrow(parquet_readr("test.parquet")),3)
